3ace7e23950bc18f523c9b902d3b64887142ebf0
Alexandre B. Simas <alexandre.impa@gmail.com>
2020-02-19 23:18:11 -0300

    Improving readability

--- Estimation/em_beta_fit.py
+++ Estimation/em_beta_fit.py
@@ -1,17 +1,9 @@
 import numpy as np
-
-# Variance function of the Binomial (same for Beta):
 from statsmodels.genmod.families.varfuncs import Binomial as VarFunc
-# Binomial family to obtain initial guesses
 from statsmodels.genmod.families import Binomial
-# Statsmodel to use GLM function for the initial guesses
 import statsmodels.api as sm
-# Special functions needed
 from scipy.special import gammaln, digamma
-# Optmization function
 from scipy.optimize import minimize
-
-# Obtaining the initial guesses
 
 
 def initial_guess(X, y, Z, link_mean, link_phi):
@@ -34,39 +26,19 @@ def initial_guess(X, y, Z, link_mean, link_phi):
     of the model. We consider the following link functions for the precision
     parameter: log, inverse_squared, inverse_power.
     '''
-    # n: Sample size
-    n = X.shape[0]
-    # q: number of covariates related to the precision parameter;
-    # that is, we have
-    # $g_phi(\phi_j) = \theta_1 + \theta_2 z_{2,j} + \cdots + \theta_q z_{q,j}$
-    q = Z.shape[1]
-    # g_mu is the link function for the mean
-    g_mu = link_mean()
-    # g_phi is the link function for the precision parameter
-    g_phi = link_phi()
-    # v_mu is the variance function of the beta distribution: V(x) = x(1-x)
-    v_mu = VarFunc()
-    # We obtain the initial estimates (guess_mean) for the parameters related
-    # to the mean by using a quasi-likelihood estimation using the GLM function.
-    guess_mean = sm.GLM(y, X, family=Binomial()).fit(disp=False).params
-    # We compute the estimated values of the mean with this new parameter
-    # in order to obtain initial guesses for the parameter related to the
-    # precision parameter
-    mu_hat = g_mu.inverse(np.dot(X, guess_mean)).reshape((n, 1))
-    # We begin by estimating the variance using a moment-like estimator
-    sigma_2_hat = np.sum((y-mu_hat)**2/(v_mu(mu_hat)))/((n-2))
-    # Here we solve the equation $\sigma^2 = 1/(1+\phi)$ in $\phi$
-    phi_hat = 1/(sigma_2_hat) - 1
-    # We then obtain an initial guess for $\theta_0$ which is the
-    # intercept of the regression related to the precision parameter
-    theta_0 = np.array([g_phi(phi_hat)])
-    # We then fill the remaining spaces with zeroes to provide
-    # the initial guess (since we only estimated the intercept).
-    guess_precision = np.concatenate([theta_0, np.zeros(q-1)])
-    # Return vector of initial guesses
-    return np.concatenate([guess_mean, guess_precision])
 
-# Q-Function (Analogous to log-likelihood function)
+    n = X.shape[0]
+    q = Z.shape[1]
+    g_mu = link_mean()
+    g_phi = link_phi()
+    v_mu = VarFunc()
+    guess_mean = sm.GLM(y, X, family=Binomial()).fit(disp=False).params
+    mu_hat = g_mu.inverse(np.dot(X, guess_mean)).reshape((n, 1))
+    sigma_2_hat = np.sum((y-mu_hat)**2/(v_mu(mu_hat)))/((n-2))
+    phi_hat = 1/(sigma_2_hat) - 1
+    theta_0 = np.array([g_phi(phi_hat)])
+    guess_precision = np.concatenate([theta_0, np.zeros(q-1)])
+    return np.concatenate([guess_mean, guess_precision])
 
 
 def QFunction(x):
@@ -108,21 +80,16 @@ def QFunction(x):
 
         Here, \Psi is the digamma function and \Gamma is the gamma function.
     '''
-    # Making thetaold global so this function can read it its values when
-    # called by the fit function
     beta = x[0:p]
     theta = x[p:(p+q)]
     mu = g_mu.inverse(np.dot(X, beta)).reshape((n, 1))
     phi = g_phi.inverse(np.dot(Z, theta)).reshape((n, 1))
 
-    # Notice that we are returning with the negative sign, since we want
-    # to maximize and this function minimizes
     return -np.sum(phi*(mu*np.log(y/(1-y))+digamma(phiold)+np.log(1-y))
                    - gammaln(mu*phi) - gammaln((1-mu)*phi)-np.log(y*(1-y))
                    - digamma(phiold)-np.log(1-y)-phiold)
 
 
-# Gradient of the Q-Function
 def gradQ(x):
     '''
     Computes the gradient of the Q-Function. This function is needed in 
@@ -155,30 +122,20 @@ def gradQ(x):
         phi parameters in the EM procedure (used in the M-step).
 
     '''
-    # Making thetaold global so this function can read it its values when
-    # called by the fit function
     beta = x[0:p]
     theta = x[p:(p+q)]
     mu = g_mu.inverse(np.dot(X, beta)).reshape((n, 1))
     phi = g_phi.inverse(np.dot(Z, theta)).reshape((n, 1))
 
-    # grad1: vector of derivatives with respect to beta (regression parameters
-    # with respect to the mean)
     grad1 = np.matmul(
         X.T, (np.log(y/(1-y))-digamma(mu*phi) +
               digamma((1-mu)*phi))*mu*(1-mu)*phi)
 
-    # grad2: vector of derivatives with respect to theta (regression parameters
-    # with respect to the precision parameter)
     grad2 = np.matmul(Z.T, (mu*np.log(y/(1-y))+digamma(phiold) +
                             np.log(1-y)-mu*digamma(mu*phi) -
                             (1-mu)*digamma((1-mu)*phi))*phi)
-    # Creating a vector of the same size as x consisting of the full vector
-    # of derivatives (that is, with respect to beta and with respect to theta)
     grad = np.zeros_like(x)
 
-    # Notice that we are returning with the negative sign, since we had
-    # to put such a sign in the Q-function.
     grad[0:p] = -grad1[:, 0]
     grad[p:(p+q)] = -grad2[:, 0]
     return grad
@@ -219,49 +176,29 @@ def fitBetaEM(X, y, Z, link_mean, link_phi, tol=10**(-6),
     $\kappa_n = (\beta_n,\theta_n)$ is the vector of regression parameters 
     at step $n$.
     '''
-    # Making phiold global so QFunction and gradQ can read its value
+
     global phiold
-    # p is the number of covariates related to the mean parameter
     p = X.shape[1]
-    # q is the number of covariates related to the precision parameter
     q = Z.shape[1]
-    # n is the sample size
     n = X.shape[0]
-    # g_mu is the link function related to the mean
     g_mu = link_mean()
-    # g_phi is the link function related to the precision parameter
     g_phi = link_phi()
-    # v_mu is the variance function of the Beta distribution: V(x) = x(1-x)
     v_mu = VarFunc()
 
-    # Obtaining initial guesses for the optmization procedure
     param_start = initial_guess(X, y, Z, link_mean, link_phi)
-    # initiating the tolerance at one to begin the loop
     tolerance = 1
-    # initiating a counter of the number of iterations
     count = 0
-    # Initialization of the thetaold variable, which is needed in
-    # both the Q-Function and gradQ. This variable comes from the EM
-    # procedure
     thetaold = param_start[p:(p+q)]
-    # Starting the loop for the EM procedure
     while(tolerance > tol and count < maxit):
-        # Computing phiold, which is needed in the QFunction and gradQ
         phiold = g_phi.inverse(np.dot(Z, thetaold)).reshape((n, 1))
-        # Doing the M-step:
         fit = minimize(QFunction, param_start,  jac=gradQ, method='BFGS')
-        # param_new: fitted parameters
         param_new = fit.x
-        # the new parameters will be the old parameters in the next iteration
         betaold = param_new[0:p]
         thetaold = param_new[p:(p+q)]
-        # Updating the tolerance
         tolerance = np.maximum(np.abs(QFunction(param_new) -
                                       QFunction(param_start)),
                                np.linalg.norm(param_new-param_start))
-        # Updating the param_start to the previous estimation
         param_start = np.concatenate([betaold, thetaold])
-        # Updating the counter
         count += 1
 
     if(count == maxit):
